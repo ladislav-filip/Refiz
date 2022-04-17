a91a3c536097c4b27c70cafc724fa3431e3eb307
LF <ladislav.filip@gmail.com>
2022-04-17 10:30:24 +0200

    Query for logon user

---
+++ src/Refiz.Queries/Entities/Queries/EntityLogonQuery.cs
@@ -0,0 +1,31 @@
+﻿#region Info
+
+// FileName:    EntityLogonQuery.cs
+// Author:      Ladislav Filip
+// Created:     17.04.2022
+
+#endregion
+
+using Refiz.Domain.AggregatesModel.EntityAggregate;
+
+namespace Refiz.Queries.Entities.Queries;
+
+public class EntityLogonQuery : EfBaseQuery<Entity, int, EntityLogon, EntityLogonFilter>
+{
+    public EntityLogonQuery(IRefizContext context, IMapper mapper) : base(context, mapper)
+    {
+    }
+
+    protected override IQueryable<Entity> ApplyFilter(EntityLogonFilter filter, IQueryable<Entity> query)
+    {
+        query = query.Where(p => p.Email.Contains(filter.Email, StringComparison.OrdinalIgnoreCase));
+        return query;
+    }
+
+    public async Task<EntityLogon?> Get(string email)
+    {
+        var filter = new EntityLogonFilter(email);
+        var (count, entityLogons) = await Get(filter);
+        return count == 0 ? null : entityLogons.First();
+    }
+}
