421681a1a65b8677c114aad30af423bf33c467fa
LF <ladislav.filip@gmail.com>
2022-04-17 09:20:01 +0200

    Add base repository and entity repository

---
+++ src/backend/Refiz.Infrastructure/Repositories/BaseRepository.cs
@@ -0,0 +1,56 @@
+#region Info
+
+// FileName:    BaseRepository.cs
+// Author:      Ladislav Filip
+// Created:     17.04.2022
+
+#endregion
+
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Refiz.Infrastructure.Repositories;
+
+public abstract class BaseRepository<TEntity, TKey>
+    where TEntity : DomainEntity<TKey>
+{
+    protected RefizContext Context { get; }
+
+    protected BaseRepository(RefizContext context)
+    {
+        Context = context;
+    }
+
+    protected Task<TEntity?> GetByIdIntenal(TKey id, params Expression<Func<TEntity, object>>[]? includePaths)
+    {
+        var query = Context.Set<TEntity>().AsQueryable();
+
+        if (includePaths is { Length: > 0 })
+        {
+            query = includePaths.Aggregate(query, (current, include) => current.Include(include));
+        }
+        
+        var result = query.FirstOrDefaultAsync(p => p.Id != null && p.Id.Equals(id));
+        return result;
+    }
+
+    public virtual Task<TEntity?> GetById(TKey id)
+    {
+        return GetByIdIntenal(id);
+    }
+
+    public virtual void Insert(TEntity ent)
+    {
+        Context.Set<TEntity>().Add(ent);
+    }
+
+    public virtual void Update(TEntity ent)
+    {
+        Context.Entry(ent).State = EntityState.Modified;
+    }
+
+    public virtual void Delete(TEntity ent)
+    {
+        Context.Set<TEntity>().Remove(ent);
+    }
+}
