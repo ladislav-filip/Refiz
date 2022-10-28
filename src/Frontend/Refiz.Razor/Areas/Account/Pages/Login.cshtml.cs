4746f5fc48e7c9a0fb83172d08ceabe9081debb3
Ladislav Filip <ladislav.filip@gmail.com>
2022-10-28 17:19:57 +0200

    Překlady a refaktor stránek

---
+++ src/Frontend/Refiz.Razor/Areas/Account/Pages/Login.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Refiz.Razor.Areas.Account.Pages;
+
+public class LoginModel : PageModel
+{
+    public class LoginRec
+    {
+        [Required]
+        public string? UserName { get; init; }
+        
+        [Required, MinLength(3)]
+        public string? Password { get; init; }
+    }
+    
+    [BindProperty] public LoginRec? Data { get; private set; }
+}
