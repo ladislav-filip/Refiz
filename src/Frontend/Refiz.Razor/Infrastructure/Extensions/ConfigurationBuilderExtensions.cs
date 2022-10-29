850c1caf1f650ca835b50f32bb48d9949f5b1c93
Ladislav Filip <ladislav.filip@gmail.com>
2022-10-29 12:35:39 +0200

    Health na databázi

---
+++ src/Frontend/Refiz.Razor/Infrastructure/Extensions/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+
+namespace Refiz.Razor.Infrastructure.Extensions;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder ConfigureCustomConfiguration(this IConfigurationBuilder confBuilder, Assembly? userSecretOfAssembly = null)
+    {
+        confBuilder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", true, reloadOnChange: true)
+            .AddJsonFile("appsettings.local.json", true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        if (userSecretOfAssembly != null)
+        {
+            confBuilder.AddUserSecrets(userSecretOfAssembly);
+        }
+
+        return confBuilder;
+    }
+}
