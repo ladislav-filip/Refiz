3cec276170632a893611cb9665b0026fac3bb1ef
Ladislav Filip <ladislav.filip@gmail.com>
2022-10-29 09:28:11 +0200

    Doplnit autentifikaci dle původního refiz řešení (#9)
    * Init razor
    
    * Překlady a refaktor stránek
    
    * Renamimg resource
    
    * Developer přihlášení
    
    * Zjednodušeno DEV spuštění
    
    * Chybová zpráva o špatném přihlášení
    
    * Uprava menu uživatele
    
    * Release režim spuštění
    
    * Odebrání ignore anti forge tokenu

---
+++ src/Frontend/Refiz.Razor/Areas/Account/Pages/Login.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Refiz.Razor.Infrastructure;
+
+namespace Refiz.Razor.Areas.Account.Pages;
+
+public class LoginModel : PageModel
+{
+    private readonly IUserManager _userManager;
+
+    public class LoginRec
+    {
+        [Required, MinLength(3)]
+        public string? UserName { get; init; }
+        
+        [Required]
+        public string? Password { get; init; }
+    }
+
+    public LoginModel(IUserManager userManager)
+    {
+        _userManager = userManager;
+    }
+    
+    [BindProperty] public LoginRec? Data { get; private set; }
+    
+    public async Task<IActionResult> OnPostAsync(LoginRec data)
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        try
+        {
+            await _userManager.SignIn(data.UserName!, data.Password!);
+        }
+        catch (AuthenticationException)
+        {
+            ModelState.AddModelError("account", ViewRes.Texts.ErrLoginFail);
+            return Page();
+        }
+
+        return RedirectToPage("Index");
+    }
+}
