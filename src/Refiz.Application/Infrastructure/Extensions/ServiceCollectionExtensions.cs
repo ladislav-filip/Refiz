3cdca9fbed98d6a8862536066afab904b1fd9b4f
Ladislav Filip <ladislav.filip@gmail.com>
2022-11-01 19:17:51 +0100

    Doplnění řešení závislostí

---
+++ src/Refiz.Application/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Refiz.Infrastructure;
+using Refiz.Queries.Infrastructure.Extensions;
+
+namespace Refiz.Application.Infrastructure.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    private const string CipherSaltKey = "CIPHER_SALT";
+    public static IServiceCollection AddCustomApplicationServices(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("Refiz");
+        serviceCollection.AddDbContext<RefizContext>(opt => opt.UseSqlServer(connectionString));
+        serviceCollection.AddScoped<IRefizContext, RefizContext>();
+        serviceCollection.AddTransient<ICipher>(_ => new Cipher(configuration.GetValue<string>(CipherSaltKey)));
+
+        serviceCollection.AddCustomQueriesServices(configuration);
+
+        var asm = Assembly.GetExecutingAssembly();
+        serviceCollection.AddAutoMapper(asm);
+
+        return serviceCollection;
+    }
+}
